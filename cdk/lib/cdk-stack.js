77a905fe803baf69a74fa5a2c18b2aff6fcb6e13
Stephen Blackstone <sblackstone@gmail.com>
2019-10-16 23:34:54 -0400

    Add deplayer

--- cdk/lib/cdk-stack.js
+++ cdk/lib/cdk-stack.js
@@ -9,13 +9,22 @@ class CdkStack extends cdk.Stack {
    * @param {string} id
    * @param {cdk.StackProps=} props
    */
+
+  addDepLayer() {
+   // Python dependencies for the lambda functions..
+   this.depLayer = new lambda.LayerVersion(this, 'depLayer', {
+     code: lambda.Code.asset('lambda/deps'),
+     compatibleRuntimes: [lambda.Runtime.PYTHON_3_6],
+   });
+
+  }
   createBucket() {
-    this.bucket = new s3.Bucket(this, 'cacheBucket', { 
+    this.bucket = new s3.Bucket(this, 'cacheBucket', {
       versioned: false,
       removalPolicy: cdk.RemovalPolicy.RETAIN
-    });    
+    });
   }
-  
+
   createLambda() {
     this.importLambda = new lambda.Function(this, 'importLambda', {
       runtime: lambda.Runtime.PYTHON_3_6,
@@ -24,19 +33,20 @@ class CdkStack extends cdk.Stack {
       timeout: cdk.Duration.seconds(900),
       //layers: [ this.depLayer],
     });
-  
-    this.bucket.grantRead(this.importLambda)
-  
-  
+
+    this.bucket.grantReadWrite(this.importLambda)
+
+
   }
-  
-  
+
+
   constructor(scope, id, props) {
     super(scope, id, props);
     this.createBucket();
     this.createLambda();
-    
-    
+    this.addDepLayer();
+
+
 
     // The code that defines your stack goes here
   }
