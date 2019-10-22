84bd887a8bab9ad4f534f9cddc33d33c0fe299fa
Stephen Blackstone <sblackstone@gmail.com>
2019-10-22 06:42:38 -0400

    Add cloudfront

--- cdk/lib/cdk-stack.js
+++ cdk/lib/cdk-stack.js
@@ -2,6 +2,7 @@ const cdk = require('@aws-cdk/core');
 const s3  = require('@aws-cdk/aws-s3');
 const s3deploy = require('@aws-cdk/aws-s3-deployment');
 const lambda = require("@aws-cdk/aws-lambda");
+const cloudfront = require("@aws-cdk/aws-cloudfront");
 
 class CdkStack extends cdk.Stack {
   /**
@@ -19,16 +20,29 @@ class CdkStack extends cdk.Stack {
    });
 
   }
+  
+  createCdn() {
+    this.distribution = new cloudfront.CloudFrontWebDistribution(this, 'websiteDist', {
+        originConfigs: [
+            {
+                s3OriginSource: {
+                    s3BucketSource: this.websiteBucket
+                },
+                behaviors : [ {isDefaultBehavior: true}]
+            }
+        ]
+     });
+  }
 
   createWebsite() {
-    const websiteBucket = new s3.Bucket(this, 'Website', {
+    this.websiteBucket = new s3.Bucket(this, 'Website', {
       websiteIndexDocument: 'index.html',
       publicReadAccess: true
     });
 
     new s3deploy.BucketDeployment(this, 'DeployWebsite', {
       sources: [s3deploy.Source.asset('../build')],
-      destinationBucket: websiteBucket,
+      destinationBucket: this.websiteBucket,
       //destinationKeyPrefix: 'web/static' // optional prefix in destination bucket
     });  }
 
@@ -67,6 +81,7 @@ class CdkStack extends cdk.Stack {
     this.createBucket();
     this.createLambda();
     this.createWebsite();
+    this.createCdn();
 
 
 
