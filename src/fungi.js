23cc43ea0b2638e9a79f14e9b335f881a839f332
Stephen Blackstone <sblackstone@gmail.com>
2019-10-25 11:52:40 -0400

    closer

--- src/fungi.js
+++ src/fungi.js
@@ -8,6 +8,7 @@ import { getFungi } from './get_fungi';
 function PageHeader(props) {
   return (
     <nav className="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
+      <a className="navbar-brand" href="#">Fungius</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
@@ -127,7 +128,7 @@ class Fungi extends React.Component {
     }
     return(
       <React.Fragment>
-      <div class="container-fluid">
+      <div className="container-fluid">
         <PageHeader resetFilters={this.resetFilters.bind(this)} visibleFungiCount={this.state.visibleFungiCount} fungiMeta={this.state.fungiMeta} fungi={this.state.fungi} filters={this.state} updateFilterSettings={this.updateFilterSettings.bind(this)} />
 
         <div className="fungi-list-container">
